df68572fa42915e7ea582c713b07a1f5fff8bb23
carvalhobruno20 <carvalho.bruno20@gmail.com>
2021-02-20 16:53:33 -0300

    Ajustes

--- scripts.js
+++ scripts.js
@@ -114,9 +114,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value) {
-        value = Number(value) * 100
+        value = value * 100
 
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
